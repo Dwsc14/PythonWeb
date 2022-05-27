0b56ca31b2e094082abeef94f0078739141e25ef
unknown <nguyen.anhduc1462@gmail.com>
2022-05-27 20:03:31 +0700

    Update 1.1

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('room/<str:pk>/', views.room)
+    path('room/<str:pk>/', views.room, name='room')
 ]
--- base/views.py
+++ base/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 
-rooms = [
-    {'id': 1, 'name': 'Phòng của Liên'},
-    {'id': 2, 'name': 'Phòng của Đức'},
-    {'id': 3, 'name': 'Phòng của Kaviex =))'},
-]
-
-
 def index(request):
+    rooms = Room.objects.all()
     content = {'rooms': rooms}
     return render(request, 'base/home.html', content)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     content = {'room': room}
     return render(request, 'base/room.html', content)
